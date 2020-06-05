4486998cef7894604804bd7fa98719999ca6fc38
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2020-06-05 15:38:25 +0300

    Added class of Complex numbers. Commit from Rider.

---
+++ NewtonsFractals/NewtonsFractals/Complex.cs
@@ -0,0 +1,22 @@
+﻿namespace NewtonsFractals
+{
+    /// <summary>
+    /// Класс комплексных чисел
+    /// </summary>
+    public class Complex
+    {
+        private double _re;
+        private double _im;
+
+        public Complex()
+        {
+            _re = _im = 0;
+        }
+
+        public Complex(double re, double im)
+        {
+            _re = re;
+            _im = im;
+        }
+    }
+}
