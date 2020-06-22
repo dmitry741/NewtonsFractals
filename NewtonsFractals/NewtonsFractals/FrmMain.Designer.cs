5adf4aae94b4f084327d31b000b5414d90f50808
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2020-06-22 13:32:57 +0300

    Change in interface

--- NewtonsFractals/NewtonsFractals/FrmMain.Designer.cs
+++ NewtonsFractals/NewtonsFractals/FrmMain.Designer.cs
@@ -29,22 +29,62 @@ namespace NewtonsFractals
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblYmax = new System.Windows.Forms.Label();
+            this.lblYmin = new System.Windows.Forms.Label();
+            this.lblXmin = new System.Windows.Forms.Label();
+            this.lblXmax = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(640, 640);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // lblYmax
+            // 
+            this.lblYmax.Location = new System.Drawing.Point(673, 12);
+            this.lblYmax.Name = "lblYmax";
+            this.lblYmax.Size = new System.Drawing.Size(49, 18);
+            this.lblYmax.TabIndex = 1;
+            this.lblYmax.Text = "label1";
+            // 
+            // lblYmin
+            // 
+            this.lblYmin.Location = new System.Drawing.Point(673, 638);
+            this.lblYmin.Name = "lblYmin";
+            this.lblYmin.Size = new System.Drawing.Size(54, 19);
+            this.lblYmin.TabIndex = 2;
+            this.lblYmin.Text = "label";
+            // 
+            // lblXmin
+            // 
+            this.lblXmin.Location = new System.Drawing.Point(15, 658);
+            this.lblXmin.Name = "lblXmin";
+            this.lblXmin.Size = new System.Drawing.Size(72, 18);
+            this.lblXmin.TabIndex = 3;
+            this.lblXmin.Text = "label3";
+            // 
+            // lblXmax
+            // 
+            this.lblXmax.Location = new System.Drawing.Point(645, 659);
+            this.lblXmax.Name = "lblXmax";
+            this.lblXmax.Size = new System.Drawing.Size(49, 18);
+            this.lblXmax.TabIndex = 4;
+            this.lblXmax.Text = "label4";
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(876, 670);
+            this.ClientSize = new System.Drawing.Size(876, 685);
+            this.Controls.Add(this.lblXmax);
+            this.Controls.Add(this.lblXmin);
+            this.Controls.Add(this.lblYmin);
+            this.Controls.Add(this.lblYmax);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -55,6 +95,10 @@ namespace NewtonsFractals
             this.ResumeLayout(false);
         }
 
+        private System.Windows.Forms.Label lblXmax;
+        private System.Windows.Forms.Label lblXmin;
+        private System.Windows.Forms.Label lblYmax;
+        private System.Windows.Forms.Label lblYmin;
         private System.Windows.Forms.PictureBox pictureBox1;
 
         #endregion
